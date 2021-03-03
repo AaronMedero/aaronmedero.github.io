e0f1740d92b551188b2b739c99f940ec3b13b6eb
Aaron D Medero <47156323+AssumetheWorst@users.noreply.github.com>
2021-03-03 15:14:36 -0500

    modified App.js to say {Aaron Medero}

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p >
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+          My name is Aaron and I'm going to master React!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
