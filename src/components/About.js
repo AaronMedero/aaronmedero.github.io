d5c9acbf2538f77ed7d42442304cb3029d63e8a7
Aaron D Medero <47156323+AssumetheWorst@users.noreply.github.com>
2021-05-26 11:05:05 -0400

    Added Footer

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ export default class About extends Component {
 
             <div className="three columns">
 
-               <img className="profile-pic"  src="images/profilepic.jpg" alt="" />
+               <img className="profile-pic"  src="C:\Users\aaron\WebDev\hw\aaronmedero.github.io\src\ProfilePic.png" alt="" />
 
             </div>
 
